831947c398faaab35683cf4ba2071315a36c784c
shishadev <zazaza008a@gmail.com>
2023-09-28 00:41:05 +0500

    basic curricula parsing has been started

--- parsers/basic_curricula/main.py
+++ parsers/basic_curricula/main.py
@@ -1,79 +1,81 @@
 import pandas as pd
+import re
+import os
 
 
 def parse(excel_file_name):
-    df = pd.read_excel(excel_file_name, sheet_name='Sheet1', skiprows=2)
+    df = pd.read_excel(excel_file_name, sheet_name='Sheet1', skiprows=1)
 
-    ep = df.iloc[0, 2]
-    educational_program = ep.replace('"', '').replace('Образовательная программа ', '')
+    speciality_code = get_speciality_code(df)
 
-    sd = df.iloc[1, 2]
-    study_direction = sd.replace('Направление ', '')
+    speciality_name = get_speciality_name(df)
 
-    sm = df.iloc[2, 2]
-    study_mode = consts.PART_TIME
-    if 'Очная' in sm:
-        study_mode = consts.FULL_TIME
-    elif 'очно' in sm.lower():
-        study_mode = consts.FULL_PART_TIME
+    programme_name = get_programme_name(df)
 
-    ct = df.iloc[3, 2]
-    competition_type = consts.TARGETED
-    if 'бюджетное' in ct:
-        competition_type = consts.BUDGET
-    elif 'по договору' in ct:
-        competition_type = consts.COMMERCIAL
+    (specialization_list, faculty) = get_specialization_and_faculty(df)
 
-    dt = df.iloc[6, 2]
-    dt = dt.replace('Время формирования: ', '')
-    date_time = utils.parse_date_string(dt)
+    enrollment_year = get_enrollment_year(df)
 
-    subject_names = self.__get_subject_names(df)
+    study_year_count = get_study_year_count(df)
 
-    applicant_list = []
-    for i in range(7, df.shape[0]):
-        insurance_number = get_insurance_number(str(df.iloc[i, 2]))
-        if insurance_number == BAD_RESULT:
-            continue
+    print(study_year_count)
 
-        agreement = self.__convert_yes_no_to_bool(df.iloc[i, 4])
-        if agreement == BAD_RESULT:
-            continue
 
-        brought_original = self.__convert_yes_no_to_bool(df.iloc[i, 6])
-        if brought_original == BAD_RESULT:
-            continue
+def get_speciality_code(df):
+    speciality_row = df.iloc[0, 0]
+    code_matching = re.search(r"\b(?:\d{2}.){2}\d{2}\b", speciality_row)
 
-        subject_scores = self.__get_subject_scores(df, i)
-        if subject_scores == BAD_RESULT:
-            continue
+    return code_matching[0]
 
-        index_after_subjects = df.shape[1] - 5
 
-        es = df.iloc[i, index_after_subjects]
-        # noinspection PyBroadException
-        try:
-            extra_score = int(es)
-        except Exception:
-            extra_score = 0
+def get_speciality_name(df):
+    speciality_row = df.iloc[0, 0]
+    name_matching = find_word_in_quotes(speciality_row)
 
-        special_right = self.__convert_yes_no_to_bool(df.iloc[i, index_after_subjects + 4])
-        if special_right == BAD_RESULT:
-            continue
+    return name_matching[0].replace("\"", "")
 
-        # noinspection PyBroadException
-        try:
-            applicant_list.append(
-                ApplicationEntry(int(df.iloc[i, 0]), insurance_number, agreement, brought_original,
-                                 subject_scores, extra_score, special_right))
-        except Exception:
-            continue
 
-    application_info = ApplicationInfo("НИУ ВШЭ", study_direction, educational_program, 4, 5, 6, study_mode,
-                                       competition_type, self._url, date_time, subject_names)
+def find_word_in_quotes(row):
+    return re.search(r"\"(?:[\da-zА-я\-]+[^\S\f\t\r\n]?)+\"", row)
 
-    return [application_info, applicant_list]
+
+def get_programme_name(df):
+    programme_row = df.iloc[1, 0]
+    programme_matching = find_word_in_quotes(programme_row)
+
+    return programme_matching[0].replace("\"", "")
+
+
+def get_specialization_and_faculty(df):
+    current_row = df.iloc[2, 0]
+
+    specialization_list = []
+
+    # noinspection SpellCheckingInspection
+    if current_row.contains("Специализ"):
+
+
+        current_row = df.iloc[2, 0]
+
+    return specialization_list, current_row.replace("Реализующее подразделение: ", "")
+
+
+def get_enrollment_year(df):
+    enrollment_row = df.iloc[3, 0]
+    year_matching = re.search(r"\d{4}/", enrollment_row)
+
+    return year_matching[0].replace("/", "")
+
+
+def get_study_year_count(df):
+    study_years_row = df.iloc[4, 0]
+    years_matching = re.search(r"\d{1,2}", study_years_row)
+
+    return years_matching[0]
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    base_files_path = "..\\..\\curricula_examples\\basic"
+
+    first_file_name = os.listdir(base_files_path)[0]
+    parse(os.path.join(base_files_path, first_file_name))

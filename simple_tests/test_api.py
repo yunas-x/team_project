adb16a33295fe0125c16e209b69c83e09701f64d
yunas-x <113031087+yunas-x@users.noreply.github.com>
2024-05-13 01:30:55 +0500

    Update test_api.py change post to get

--- simple_tests/test_api.py
+++ simple_tests/test_api.py
@@ -6,36 +6,36 @@ api_token = os.getenv("API_TOKEN")
 
 def test_programs():
 
-    r = requests.post(  
+    r = requests.get(  
                         f"{api_url}/programs",
                         headers={"Authorization": f"Bearer {api_token}"}
-                      )
+                    )
     
     assert r.status_code == 200
 
 def test_fields():
 
-    r = requests.post(  
+    r = requests.get(  
                         f"{api_url}/fields",
                         headers={"Authorization": f"Bearer {api_token}"}
-                      )
+                    )
     
     assert r.status_code == 200
     
 def test_universities():
 
-    r = requests.post(  
+    r = requests.get(  
                         f"{api_url}/universities",
                         headers={"Authorization": f"Bearer {api_token}"}
-                      )
+                    )
     
     assert r.status_code == 200
     
 def test_infographics():
 
-    r = requests.post(  
+    r = requests.get(  
                         f"{api_url}/infographics?first_program_id=1&second_program_id=2",
                         headers={"Authorization": f"Bearer {api_token}"}
-                      )
+                    )
     
     assert r.status_code == 200
